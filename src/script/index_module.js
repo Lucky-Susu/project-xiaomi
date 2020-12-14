b6593051d96a8bd6c271b8b72d47469ac98d5611
Lucky-Susu <1375056101@qq.com>
2020-12-14 22:43:00 +0800

    首页js基本完成”

--- src/script/index_module.js
+++ src/script/index_module.js
@@ -1,55 +1,123 @@
 define([], () => {
     return {
         init: function() {
-            //代码实现
-            //1.鼠标移入左侧的li元素，显示右侧的大盒子。
-            const $list = $('.menu li');
-            const $cartlist = $('.cartlist');
+            const $goods = $('.main-r');
+            const $lunbo = $('.banner-show');
+            const $ulist = $('.sw-piclist');
+            const $piclist = $('.sw-piclist li');
+            const $circle = $('.sw-btnlist li');
+            const $left = $('#show-left');
+            const $right = $('#show-right');
+            const $list = $('.left-nav-li');
+            const $bitem = $('.banner-list');
             const $items = $('.item');
+            let $timer = null;
+            let $num = 0;
+            // 数据渲染
+            $.ajax({
+                url: 'http://localhost:8080/dashboard/project-xiaomi/php/listdata.php',
+                dataType: 'json'
+            }).done(function(data) {
+                let $strhtml = '';
+                $.each(data, function(index, value) {
+                    if (index < 8) {
+                        $strhtml += `
+                        <div class="sj goods-sj">
+                            <a href="javascript:;">
+                                <img class="list" src="${value.url}"/>
+                                <h3 class="tit">${value.title}</h3>
+                                <p class="con">${value.describe}</p>
+                                <p class="price">￥${value.price}</p>
+                            </a>
+                        </div>
+                    `;
+                    }
+                });
+                $goods.html($strhtml);
+            })
+
+
+
+            // 轮播图
+            const $liwidth = $piclist.first().width();
+            $ulist.width($liwidth * $piclist.size());
+            $circle.on('click', function() {
+                $num = $(this).index() - 1;
+                capsulation()
+            })
+            $lunbo.hover(function() {
+                clearInterval($timer);
+                $left.show();
+                $right.show();
+            }, function() {
+                $left.hide();
+                $right.hide();
+                $timer = setInterval(function() {
+                    $right.click();
+                }, 3000);
+            })
+            $left.on('click', function() {
+                $num -= 2;
+                capsulation()
+            })
+
+            $right.on('click', function() {
+                capsulation()
+            })
+
+            function capsulation() {
+                $num++;
+                if ($num === $circle.size() + 1) {
+                    $ulist.css('left', 0);
+                    $num = 1;
+                }
+
+                if ($num === -1) {
+                    $ulist.css('left', -$liwidth * $circle.size());
+                    $num = $circle.size() - 1;
+                }
+
+                if ($num === $circle.size()) {
+                    $circle.first().addClass('active').siblings('li').removeClass('active');
+                } else {
+                    $circle.eq($num).addClass('active').siblings('li').removeClass('active');
+                }
+                $ulist.stop(true).animate({
+                    left: -$liwidth * $num
+                })
+            }
+            $timer = setInterval(function() {
+                $right.click();
+            }, 3000);
+
+
+            // 二级导航
             $list.hover(function() {
-                $cartlist.show();
+                $bitem.show();
                 $(this).addClass('active').siblings('li').removeClass('active');
-                //切换内容发生改变，不同的li对应不同的内容块。
                 $items.eq($(this).index()).show().siblings('.item').hide();
 
-                //改变右侧的大盒子的位置
                 let $scrolltop = $(window).scrollTop();
                 let $bannertop = $('.banner').offset().top;
                 if ($scrolltop > $bannertop) {
-                    $cartlist.css({
+                    $bitem.css({
                         top: $scrolltop - $bannertop
                     });
                 } else {
-                    $cartlist.css({
+                    $bitem.css({
                         top: 0
                     });
                 }
             }, function() {
-                $cartlist.hide();
+                $bitem.hide();
+                $list.removeClass('active');
             });
 
-            //2.鼠标移入右侧的大盒子，大盒子依然显示隐藏
-            $cartlist.hover(function() {
+            $bitem.hover(function() {
                 $(this).show();
             }, function() {
                 $(this).hide();
             });
-
-
-            //检测是否用户已经登录
-            if (localStorage.getItem('loginname')) {
-                $('.admin').show();
-                $('.login').hide();
-                $('.admin span').html(localStorage.getItem('loginname'));
-            }
-
-            //退出登录 - 删除本地存储
-            $('.admin a').on('click', function() {
-                $('.admin').hide();
-                $('.login').show();
-                localStorage.removeItem('loginname');
-            });
-
         }
     }
-});
+})
