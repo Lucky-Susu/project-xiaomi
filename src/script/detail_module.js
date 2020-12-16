f10c2c1d532d890e9ad0e728ee74f0e0e66bc3bb
Lucky-Susu <1375056101@qq.com>
2020-12-16 22:31:08 +0800

    12月16日提交，商品详情页已完成

--- src/script/detail_module.js
+++ src/script/detail_module.js
@@ -3,73 +3,103 @@ define(['jcookie'], () => {
         init: function() {
             //1.通过地址栏获取列表页面传入的sid。
             let $sid = location.search.substring(1).split('=')[1];
+
             if (!$sid) {
                 $sid = 1;
             }
-            //2.将sid传给后端，后端根据对应的sid返回不同的数据。
+
+            //2.将sid传给后端
             $.ajax({
-                url: 'http://localhost/JS2010/week06/Day%2029-Day%2031_jquery/projectname/php/detail.php',
+                url: 'http://192.168.64.2//dashboard/project-xiaomi/php/detail.php',
                 data: {
                     sid: $sid
                 },
                 dataType: 'json'
             }).done(function(data) {
-                console.log(data);
-                console.log(data.urls);
-                //获取数据，将数据放入对应的结构中。
                 $('#smallpic').attr('src', data.url);
                 $('.loadtitle').html(data.title);
                 $('.loadpcp').html(data.price);
+            });
 
-                //渲染放大镜下面的小图
-                let $picurl = data.urls.split(','); //将数据转换成数组。
-                let $strhtml = '';
-                const $list = $('#list');
-                console.log($picurl);
-                $.each($picurl, function(index, value) {
-                    $strhtml += `
-                <li>
-                    <img src="${value}"/>
-                </li>
-            `;
-                });
-                $list.html($strhtml);
+            //3.图片轮播效果
+
+            const $left = $('#left');
+            const $right = $('#right');
+            const $lunbo = $('.wrap') //盒子
+            const $picList = $('.spic-img img');
+            const $btnList = $('#olist li');
+            let timer = null;
+            let $num = 0;
+
+
+
+            $btnList.on('mouseover', function() {
+                $num = $(this).index();
+                lunbo()
+            });
+
+            function lunbo() {
+                $btnList.eq($num).addClass('active').siblings('li').removeClass('active');
+                $picList.eq($num).css('opacity', 1).siblings('img').css('opacity', 0);
+            }
+
+            $right.on('click', function() {
+                $num++;
+                if ($num > $btnList.size() - 1) {
+                    $num = 0;
+                }
+                lunbo();
+            })
+
+
+            $left.on('click', function() {
+                $num--;
+                if ($num < 0) {
+                    $num = $btnList.size() - 1;
+                }
+                lunbo();
             });
 
 
+            $lunbo.hover(function() {
+                clearInterval(timer);
 
-            //五.购物车：(商品sid、商品数量)
-            //1.设置存储cookie的变量。
-            let arrsid = []; //存储商品的sid
-            let arrnum = []; //存储商品的数量
-            //2.判断是第一次存储，多次存储。
-            //获取cookie才能判断，每存储一个商品对应的cookie就会发生变化。
-            //提前预判cookie设置时的key值(cookiesid/cookienum)
-            function getcookietoarray() {
+            }, function() {
+                timer = setInterval(function() {
+                    $right.click();
+                }, 3000);
+            });
+
+            timer = setInterval(function() {
+                $right.click();
+            }, 3000);
+
+            // cookie取值
+            let arrsid = [];
+            let arrnum = [];
+
+            function getcookiesid() {
                 if ($.cookie('cookiesid') && $.cookie('cookienum')) {
                     arrsid = $.cookie('cookiesid').split(',');
                     arrnum = $.cookie('cookienum').split(',');
                 }
             }
-            //上面的函数获取cookie值，并且转换成数组，方便判断是否是第一次。
-            //第一次存储添加sid进入arrsid，存储数量
-            //第二次以上，直接修改数量。
+
             $('.p-btn a').on('click', function() {
-                getcookietoarray(); //获取cookie，变成数组，判断是否存在。
-                if ($.inArray($sid, arrsid) === -1) { //不存在
+                getcookiesid();
+                if ($.inArray($sid, arrsid) === -1) {
                     arrsid.push($sid);
-                    $.cookie('cookiesid', arrsid, { expires: 10, path: '/' });
+                    $.cookie('cookiesid', arrsid, { expires: 5, path: '/' });
                     arrnum.push($('#count').val());
-                    $.cookie('cookienum', arrnum, { expires: 10, path: '/' });
-                } else { //存着
-                    //通过$sid获取商品的数量所在的位置。
+                    $.cookie('cookienum', arrnum, { expires: 5, path: '/' });
+                } else {
+
                     let $index = $.inArray($sid, arrsid);
-                    // arrnum[$index]//原来的数组
-                    // $('#count').val()//新添加的数量
+
                     arrnum[$index] = parseInt(arrnum[$index]) + parseInt($('#count').val()); //重新赋值
-                    $.cookie('cookienum', arrnum, { expires: 10, path: '/' });
+                    $.cookie('cookienum', arrnum, { expires: 3, path: '/' });
                 }
-                alert('按钮被点击了');
+                confirm('你确定要加入购物车吗？');
             });
         }
     }
--- src/script/registry.js
+++ src/script/registry.js
@@ -14,167 +14,163 @@
     let $emailflag = true;
 
     $username.on('focus', function() {
-        $hint[0].html('中英文均可，最长14个英文或7个汉字');
-        $hint[0].css('color', '#999');
+        $hint.first().html('中英文均可，最长14个英文或7个汉字');
+        $hint.first().css('color', '#999');
     })
 
     $username.on('blur', function() {
-        if ($(this).value !== '') {
-            let $strlen = $(this).value.replace(/[\u4e00-\u9fa5]/g, '**').length;
+        if ($(this).val() !== '') {
+            let $strlen = $(this).val().replace(/[\u4e00-\u9fa5]/g, '**').length;
             if ($strlen > 0 && $strlen <= 14) {
                 let $reg = /^[a-zA-Z\u4e00-\u9fa5]+$/;
-                if ($reg.test($(this).value)) {
-                    $span[0].html('√');
-                    $span[0].css('color', 'green');
+                if ($reg.test($(this).val())) {
+                    $span.eq(0).html('√');
+                    $span.eq(0).css('color', 'green');
                     $userflag = true;
                 } else {
-                    $hint[0].html('你输入的格式有误');
-                    $hint[0].css('color', 'red');
+                    $hint.eq(0).html('你输入的格式有误');
+                    $hint.eq(0).css('color', 'red');
                     $userflag = false;
                 }
             } else {
-                $hint[0].html('用户名的长度有误');
-                $hint[0].css('color', 'red');
+                $hint.eq(0).html('用户名的长度有误');
+                $hint.eq(0).css('color', 'red');
                 $userflag = false;
             }
         } else {
-            $hint[0].html('用户名不能为空');
-            $hint[0].css('color', 'red');
+            $hint.eq(0).html('用户名不能为空');
+            $hint.eq(0).css('color', 'red');
             $userflag = false;
         }
     })
 
     $telphone.on('focus', function() {
-        $hint[3].html('请输入11位正确的手机号码');
-        $hint[3].css('color', '#999');
+        $hint.eq(3).html('请输入11位正确的手机号码');
+        $hint.eq(3).css('color', '#999');
     })
     $telphone.on('blur', function() {
-        if ($(this).value !== '') {
+        if ($(this).val() !== '') {
             let $reg = /^1[3|5|8]\d{9}$/;
-            if ($reg.test($(this).value)) {
-                $span[3].html('√');
-                $span[3].css('color', 'green');
+            if ($reg.test($(this).val())) {
+                $span.eq(3).html('√');
+                $span.eq(3).css('color', 'green');
                 $telpflag = true;
             } else {
-                $hint[3].html('手机号码格式有误');
-                $hint[3].css('color', 'red');
+                $hint.eq(3).html('手机号码格式有误');
+                $hint.eq(3).css('color', 'red');
                 $telpflag = false;
             }
         } else {
-            $hint[3].html('手机号码不能为空');
-            $hint[3].css('color', 'red');
+            $hint.eq(3).html('手机号码不能为空');
+            $hint.eq(3).css('color', 'red');
             $telpflag = false;
         }
     })
 
     $password.on('focus', function() {
-        $hint[1].html('请输入密码，长度为8-14个字符');
-        $hint[1].css('color', '#999');
+        $hint.eq(1).html('请输入密码，长度为8-14个字符');
+        $hint.eq(1).css('color', '#999');
     })
     $password.on('blur', function() {
-        if ($(this).value !== '') {
-            if ($(this).value.length >= 0 && $(this).value.length <= 14) {
+        if ($(this).val() !== '') {
+            if ($(this).val().length >= 0 && $(this).val().length <= 14) {
                 let $regnum = /\d+/;
                 let $reguppercase = /[A-Z]+/;
                 let $reglowercase = /[a-z]+/;
                 let $other = /[\W_]+/;
                 let $count = 0;
-                if ($regnum.test($(this).value)) {
+                if ($regnum.test($(this).val())) {
                     $count++
                 }
-                if ($reguppercase.test($(this).value)) {
+                if ($reguppercase.test($(this).val())) {
                     $count++
                 }
-                if ($reglowercase.test($(this).value)) {
+                if ($reglowercase.test($(this).val())) {
                     $count++
                 }
-                if ($other.test($(this).value)) {
+                if ($other.test($(this).val())) {
                     $count++
                 }
 
                 switch ($count) {
                     case 1:
-                        $span[1].html('弱');
-                        $span[1].css('color', 'red');
+                        $span.eq(1).html('弱');
+                        $span.eq(1).css('color', 'red');
                         $passflag = false;
                         break;
                     case 2:
-                        $span[1].html('中');
-                        $span[1].css('color', 'orange');
+                        $span.eq(1).html('中');
+                        $span.eq(1).css('color', 'orange');
                         $passflag = true;
                         break;
                     case 3:
                     case 4:
-                        $span[1].html('强');
-                        $span[1].css('color', 'green');
+                        $span.eq(1).html('强');
+                        $span.eq(1).css('color', 'green');
                         $passflag = true;
                         break;
                 }
             } else {
-                $hint[1].html('密码的长度有误');
-                $hint[1].css('color', 'red');
+                $hint.eq(1).html('密码的长度有误');
+                $hint.eq(1).css('color', 'red');
                 $passflag = false;
             }
         } else {
-            $hint[1].html('用户密码不能为空');
-            $hint[1].css('color', 'red');
+            $hint.eq(1).html('用户密码不能为空');
+            $hint.eq(1).css('color', 'red');
             $passflag = false;
         }
     })
 
     $email.on('focus', function() {
-        $hint[2].html('请输入电子邮箱');
-        $hint[2].css('color', '#999');
+        $hint.eq(2).html('请输入电子邮箱');
+        $hint.eq(2).css('color', '#999');
     })
     $email.on('blur', function() {
-        if ($(this).value !== '') {
+        if ($(this).val() !== '') {
             let $reg = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
-            if ($reg.test($(this).value)) {
-                $span[2].html('√');
-                $span[2].css('color', 'green');
+            if ($reg.test($(this).val())) {
+                $span.eq(2).html('√');
+                $span.eq(2).css('color', 'green');
                 $emailfalg = true;
             } else {
-                $hint[2].html('您输入的邮箱有误');
-                $hint[2].css('color', 'red');
+                $hint.eq(2).html('您输入的邮箱有误');
+                $hint.eq(2).css('color', 'red');
                 $emailflag = false;
             }
         } else {
-            $hint[2].html('电子邮箱不能为空');
-            $hint[2].css('color', 'red');
+            $hint.eq(2).html('电子邮箱不能为空');
+            $hint.eq(2).css('color', 'red');
             $emailflag = false;
         }
     })
 
     // 4、 阻止跳转
-    $form.on('$submit', function() {
-        if ($username.value === '') {
-            $hint[0].html('用户名不能为空');
-            $hint[0].css('color', 'red');
+    $submit.on('click', function() {
+        if ($username.val() === '') {
+            $hint.eq(0).html('用户名不能为空');
+            $hint.eq(0).css('color', 'red');
             $userflag = false;
         }
-        if ($telphone.value === '') {
-            $hint[3].html('电话号码不能为空');
-            $hint[3].css('color', 'red');
+        if ($telphone.val() === '') {
+            $hint.eq(3).html('电话号码不能为空');
+            $hint.eq(3).css('color', 'red');
             telpflag = false;
         }
-        if ($password.value === '') {
-            $hint[1].html('用户密码不能为空');
-            $hint[1].css('color', 'red');
+        if ($password.val() === '') {
+            $hint.eq(1).html('用户密码不能为空');
+            $hint.eq(1).css('color', 'red');
             $passflag = false;
         }
-        if ($email.value === '') {
-            $hint[2].html('电子邮箱不能为空');
-            $hint[2].css('color', 'red');
+        if ($email.val() === '') {
+            $hint.eq(2).html('电子邮箱不能为空');
+            $hint.eq(2).css('color', 'red');
             $emailflag = false;
         }
         if (!$userflag || !$telpflag || !$passflag || !$emailflag) {
             return false;
         }
     })
-
-
-
-
 
     $username.on('blur', function() {
         $.ajax({
@@ -191,5 +187,4 @@
             }
         });
     });
-
 }(jQuery);
